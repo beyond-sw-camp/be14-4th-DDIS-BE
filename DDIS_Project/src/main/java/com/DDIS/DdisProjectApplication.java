40f27f876d51df83d20c62fdc3f40a4edb493f5d
rrangeess <zlions2127@gmail.com>
2025-04-24 11:29:51 +0900

    project-setup

---
+++ DDIS_Project/src/main/java/com/DDIS/DdisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.DDIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdisProjectApplication.class, args);
+	}
+
+}
