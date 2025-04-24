cf0f9df5d93ee9e70575c9b8e74a33c8c690cd26
rrangeess <zlions2127@gmail.com>
2025-04-25 03:38:12 +0900

    [feat] 회원 가입 기능 구현

---
+++ DDIS_Project/src/main/java/com/DDIS/client/Command/domain/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.DDIS.client.Command.domain.repository;
+
+import com.DDIS.client.Command.domain.aggregate.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<UserEntity, Long> {
+    Optional<Object> findByClientId(String clientId);
+}
