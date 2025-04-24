06efbc3839bae009019d2da8a2d6a35b83bf289e
SJ <jerry20011214@gmail.com>
2025-04-25 05:49:34 +0900

    공동방 DTO 구현

---
+++ DDIS_Project/src/main/java/com/DDIS/shareTodo/Command/application/dto/CreateShareRoomDTO.java
@@ -0,0 +1,12 @@
+package com.DDIS.shareTodo.Command.application.dto;
+
+public class CreateShareRoomDTO {
+    private int memberCount;
+    private boolean isActive;
+    private String title;
+    private String content;
+    private String colorRgb;
+    private int approveRequiredCount;
+    private String startDate;
+    private String endDate;
+}
