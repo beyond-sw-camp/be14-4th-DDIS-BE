c7f97209243dfcedda3e3dc977ce48751854be05
RKDLDE <kangcanddy@gmail.com>
2025-04-26 07:31:38 +0900

    feat: 특정 회원의 카테고리 별 todo 조회

---
+++ DDIS_Project/src/main/resources/sql/personalTodo/personalTodo_categoryTodo_select.sql
@@ -0,0 +1,8 @@
+# 특정 회원의 카테고리 별 투두 조회
+
+SELECT pt.*, pc.personal_category_name, ptd.todo_date
+FROM personal_todos pt
+         LEFT JOIN personal_categories pc ON pt.personal_category_num = pc.personal_category_num
+         LEFT JOIN personal_todo_date ptd ON pt.todo_num = ptd.todo_num
+WHERE pt.client_num = ?
+  AND pt.personal_category_num = ?;
