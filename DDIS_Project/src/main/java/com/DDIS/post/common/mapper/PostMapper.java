e9107b01e26bc18ae39fd1367b6f3b44c18699ec
Jooahyeon <dkgus0496@naver.com>
2025-04-26 13:33:04 +0900

    [fix] postMapper 패키지 구조 변경

--- DDIS_Project/src/main/java/com/DDIS/post/common/mapper/PostMapper.java
+++ DDIS_Project/src/main/java/com/DDIS/post/common/mapper/PostMapper.java
@@ -1,6 +1,6 @@
 // Java에서 SQL을 호출할 인터페이스 정의
 
-package com.DDIS.post.Query.mapper;
+package com.DDIS.post.common.mapper;
 
 import com.DDIS.post.Query.dto.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
