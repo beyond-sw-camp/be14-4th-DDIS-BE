5f42600c7f0a02d72fce78b9b0d5387ef0c0e421
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-26 22:30:35 +0900

    feat: Follow DTO 설정

---
+++ DDIS_Project/src/main/java/com/DDIS/follow/Query/dto/FollowQueryDTO.java
@@ -0,0 +1,22 @@
+package com.DDIS.follow.Query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.apache.ibatis.type.Alias;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Alias("ClientQueryDTO")
+public class FollowQueryDTO {
+
+    private Long followNum;
+    private Long followerNum;
+    private Long followingNum;
+
+    private Long clientNum;
+    private String clientNickname;
+}
