4f13e2e0cf48f3d6b012442656057cc374b9ebfe
RKDLDE <kangcanddy@gmail.com>
2025-04-26 19:45:36 +0900

    refactor: isPublic personal_todo_date로 이전

--- DDIS_Project/src/main/java/com/DDIS/personalTodo/Query/dto/PersonalTodoQueryDTO.java
+++ DDIS_Project/src/main/java/com/DDIS/personalTodo/Query/dto/PersonalTodoQueryDTO.java
@@ -14,7 +14,6 @@ public class PersonalTodoQueryDTO {
     private String todoContent;
     private Long personalCategoryNum;
     private Long clientNum;
-    private Boolean isPublic;
 
     // 조인해서 가져오는 값들
     private String personalCategoryName; // 카테고리 이름
@@ -22,4 +21,5 @@ public class PersonalTodoQueryDTO {
     private String todoDate; // 할 일 날짜
     private Boolean isDone; // 완료 여부
     private Integer pinOrder; // 핀 순서
+    private Boolean isPublic; // 공개여부
 }
--- DDIS_Project/src/main/resources/sql/personalTodo/personalTodo_privateTodo_select.sql
+++
@@ -1,7 +0,0 @@
-# 공개 여부가 비공개(false)인 특정 회원의 todo 조회
-
-SELECT pt.*, ptd.todo_date
-FROM personal_todos pt
-         JOIN personal_todo_date ptd ON pt.todo_num = ptd.todo_num
-WHERE pt.client_num = ?
-  AND pt.is_public = FALSE;
