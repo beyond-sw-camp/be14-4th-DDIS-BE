fd6218bdc9712b93b4ceeea76631cd48e938ec51
RKDLDE <kangcanddy@gmail.com>
2025-04-26 16:27:38 +0900

    refactor: response entity 전환

--- DDIS_Project/src/main/java/com/DDIS/personalTodo/Query/controller/PersonalTodoQueryController.java
+++ DDIS_Project/src/main/java/com/DDIS/personalTodo/Query/controller/PersonalTodoQueryController.java
@@ -3,6 +3,7 @@ package com.DDIS.personalTodo.Query.controller;
 import com.DDIS.personalTodo.Query.dto.PersonalTodoQueryDTO;
 import com.DDIS.personalTodo.Query.service.PersonalTodoQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,41 +17,47 @@ public class PersonalTodoQueryController {
 
     // 1. 회원별 전체 todo 조회
     @GetMapping("/{clientNum}")
-    public List<PersonalTodoQueryDTO> getAllTodos(@PathVariable Long clientNum) {
-        return personalTodoQueryService.getAllTodos(clientNum);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getAllTodos(@PathVariable Long clientNum) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getAllTodos(clientNum);
+        return ResponseEntity.ok(todos);
     }
 
     // 2. 특정 일자별 특정 회원의 todo 조회
     @GetMapping("/{clientNum}/date")
-    public List<PersonalTodoQueryDTO> getTodosByDate(@PathVariable Long clientNum,
-                                             @RequestParam String todoDate) {
-        return personalTodoQueryService.getTodosByDate(clientNum, todoDate);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getTodosByDate(@PathVariable Long clientNum,
+                                                                     @RequestParam String todoDate) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getTodosByDate(clientNum, todoDate);
+        return ResponseEntity.ok(todos);
     }
 
     // 3. 카테고리 별 특정 회원의 todo 조회
     @GetMapping("/{clientNum}/category/{categoryNum}")
-    public List<PersonalTodoQueryDTO> getTodosByCategory(@PathVariable Long clientNum,
-                                                 @PathVariable Long categoryNum) {
-        return personalTodoQueryService.getTodosByCategory(clientNum, categoryNum);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getTodosByCategory(@PathVariable Long clientNum,
+                                                                         @PathVariable Long categoryNum) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getTodosByCategory(clientNum, categoryNum);
+        return ResponseEntity.ok(todos);
     }
 
     // 4. 핀 고정된 todo 조회
     @GetMapping("/{clientNum}/pinned")
-    public List<PersonalTodoQueryDTO> getPinnedTodos(@PathVariable Long clientNum) {
-        return personalTodoQueryService.getPinnedTodos(clientNum);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getPinnedTodos(@PathVariable Long clientNum) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getPinnedTodos(clientNum);
+        return ResponseEntity.ok(todos);
     }
 
     // 5, 6번 공개/비공개 TODO 조회
     @GetMapping("/{clientNum}/visibility")
-    public List<PersonalTodoQueryDTO> getTodosByVisibility(@PathVariable Long clientNum,
-                                                           @RequestParam Boolean isPublic) {
-        return personalTodoQueryService.getTodosByVisibility(clientNum, isPublic);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getTodosByVisibility(@PathVariable Long clientNum,
+                                                                           @RequestParam Boolean isPublic) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getTodosByVisibility(clientNum, isPublic);
+        return ResponseEntity.ok(todos);
     }
 
     // 7, 8번 완료/미완료 된 todo 조회
     @GetMapping("/{clientNum}/donestatus")
-    public List<PersonalTodoQueryDTO> getTodosByDoneStatus(@PathVariable Long clientNum,
-                                                           @RequestParam Boolean isDone) {
-        return personalTodoQueryService.getTodosByDoneStatus(clientNum, isDone);
+    public ResponseEntity<List<PersonalTodoQueryDTO>> getTodosByDoneStatus(@PathVariable Long clientNum,
+                                                                           @RequestParam Boolean isDone) {
+        List<PersonalTodoQueryDTO> todos = personalTodoQueryService.getTodosByDoneStatus(clientNum, isDone);
+        return ResponseEntity.ok(todos);
     }
 }
