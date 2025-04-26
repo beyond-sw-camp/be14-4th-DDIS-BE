32774aefc4bf648430e18c309e1ee628586a46e6
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-27 05:21:56 +0900

    feat: 신고 처리 기능 구현

--- DDIS_Project/src/main/java/com/DDIS/report/Command/application/service/ReportCommandServiceImpl.java
+++ DDIS_Project/src/main/java/com/DDIS/report/Command/application/service/ReportCommandServiceImpl.java
@@ -1,5 +1,10 @@
 package com.DDIS.report.Command.application.service;
 
+import com.DDIS.client.Command.domain.aggregate.ClientEntity;
+import com.DDIS.client.Command.domain.repository.ClientRepository;
+import com.DDIS.post.Command.domain.aggregate.entity.Client;
+import com.DDIS.post.Command.domain.aggregate.entity.Post;
+import com.DDIS.post.Command.domain.repository.PostRepository;
 import com.DDIS.report.Command.application.dto.ReportRegistRequest;
 import com.DDIS.report.Command.domain.aggregate.entity.ReportEntity;
 import com.DDIS.report.Command.domain.repository.ReportRepository;
@@ -16,8 +21,8 @@ import java.time.format.DateTimeFormatter;
 public class ReportCommandServiceImpl implements ReportCommandService {
 
     private final ReportRepository reportRepository;
-//    private final ClientRepository clientRepository;
-//    private final PostRepository postRepository;
+    private final ClientRepository clientRepository;
+    private final PostRepository postRepository;
 //    private final CommentRepository commentRepository;
 
     @Override
@@ -48,34 +53,34 @@ public class ReportCommandServiceImpl implements ReportCommandService {
         reportRepository.save(report);
 
         // 승인일 때 누적 처리
-//        if (accept) {
-//            Long targetClientNum = null;
-//            switch (report.getReportType()) {
-//                case "client":
-//                    targetClientNum = report.getReportTypeNum();
-//                    break;
-//                case "post":
-//                    Post post = postRepository.findById(report.getReportTypeNum())
-//                            .orElseThrow(() -> new RuntimeException("게시글 없음"));
-//                    targetClientNum = post.getClientNum();
-//                    break;
+        if (accept) {
+            Long targetClientNum = null;
+            switch (report.getReportType()) {
+                case "client":
+                    targetClientNum = report.getReportTypeNum();
+                    break;
+                case "post":
+                    Post post = postRepository.findById(report.getReportTypeNum())
+                            .orElseThrow(() -> new RuntimeException("게시글 없음"));
+                    targetClientNum = post.getClientNum().getClientNum();
+                    break;
 //                case "comment":
 //                    Comment comment = commentRepository.findById(report.getReportTypeNum())
 //                            .orElseThrow(() -> new RuntimeException("댓글 없음"));
 //                    targetClientNum = comment.getClientNum();
 //                    break;
-//            }
-//            if (targetClientNum != null) {
-//                Client client = clientRepository.findById(targetClientNum)
-//                        .orElseThrow(() -> new RuntimeException("회원 없음"));
-//                int count = client.getClientReportNum() + 1;
-//                client.setClientReportNum(count);
-//                if (count >= 3) {
-//                    client.setClientStatus(false);
-//                }
-//                clientRepository.save(client);
-//            }
-//        }
+            }
+            if (targetClientNum != null) {
+                ClientEntity client = clientRepository.findById(targetClientNum)
+                        .orElseThrow(() -> new RuntimeException("회원 없음"));
+                int count = client.getClientReportNum() + 1;
+                client.setClientReportNum(count);
+                if (count >= 3) {
+                    client.setClientAccountStatus("DEACTIVATE");
+                }
+                clientRepository.save(client);
+            }
+        }
     }
 
 }
