f3e9b90c0e7f98f5279c1c24d30af4be1ed0058c
SJ <jerry20011214@gmail.com>
2025-04-27 00:44:26 +0900

    오류 수정

--- DDIS_Project/src/main/java/com/DDIS/post/Command/application/service/PostServiceImpl.java
+++ DDIS_Project/src/main/java/com/DDIS/post/Command/application/service/PostServiceImpl.java
@@ -1,37 +1,37 @@
-//// 구현체 실제 로직 작성
-//package com.DDIS.post.Command.application.service;
-//
-//import com.DDIS.post.Command.domain.aggregate.entity.Post;
-//import com.DDIS.post.Command.domain.repository.PostRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//@Service("commandPostServiceImpl")
-//@RequiredArgsConstructor
-//@Slf4j
-//public class PostServiceImpl implements PostService {
-//
-//        private final PostRepository postRepository;
-//
-//    @Override
-//    public Post getPost(Long postNum, String inputPassword) {
-//        Post post = postRepository.findById(postNum)
-//                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-//
-//        if (Boolean.FALSE.equals(post.getIsPublic())) {
-//            if (inputPassword == null) {
-//                throw new IllegalArgumentException("비밀번호를 입력해주세요.");
-//            }
-//
-//            String savedPassword = post.getPostPassword();
-//            if (savedPassword == null || !savedPassword.trim().equals(inputPassword.trim())) {
-//                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-//            }
-//        }
-//
-//        return post;
-//    }
-//
-//
-//}
+// 구현체 실제 로직 작성
+package com.DDIS.post.Command.application.service;
+
+import com.DDIS.post.Command.domain.aggregate.entity.Post;
+import com.DDIS.post.Command.domain.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service("commandPostServiceImpl")
+@RequiredArgsConstructor
+@Slf4j
+public class PostServiceImpl implements PostService {
+
+        private final PostRepository postRepository;
+
+    @Override
+    public Post getPost(Long postNum, String inputPassword) {
+        Post post = postRepository.findById(postNum)
+                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+
+        if (Boolean.FALSE.equals(post.getIsPublic())) {
+            if (inputPassword == null) {
+                throw new IllegalArgumentException("비밀번호를 입력해주세요.");
+            }
+
+            String savedPassword = post.getPostPassword();
+            if (savedPassword == null || !savedPassword.trim().equals(inputPassword.trim())) {
+                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            }
+        }
+
+        return post;
+    }
+
+
+}
